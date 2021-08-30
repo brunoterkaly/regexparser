fecea5f381a5e8ffa82542f10659c57b9ef7dacb
Bruno Terkaly <brunoterkaly@hotmail.com>
2021-08-29 21:35:07 -0700

    Updates to parser

--- regex/src/parser.py
+++ regex/src/parser.py
@@ -1,4 +1,5 @@
 from io import StringIO
+import copy
 import json
 import pprint
 import sys
@@ -7,7 +8,7 @@ import os
 
 from subprocess import Popen, PIPE, STDOUT
 
-'''
+"""
 
 <expr> ::= <term>
     | <term>'|'<expr>
@@ -20,49 +21,67 @@ from subprocess import Popen, PIPE, STDOUT
 <char> ::= <any-char-except-meta>
     | '\'<any-char>
 <meta-char> ::= '?' | '*' | '+'
-'''
+"""
 
 # ===============================================================
 # https://deniskyashif.com/2020/08/17/parsing-regex-with-recursive-descent/
 # ===============================================================
 class MyStack:
-     def __init__(self):
-         self.container = []  # You don't want to assign [] to self - when you do that, you're just assigning to a new local variable called `self`.  You want your stack to *have* a list, not *be* a list.
+    def __init__(self):
+        self.container = (
+            []
+        )  # You don't want to assign [] to self - when you do that, you're just assigning to a new local variable called `self`.  You want your stack to *have* a list, not *be* a list.
 
-     def isEmpty(self):
-         return self.size() == 0   # While there's nothing wrong with self.container == [], there is a builtin function for that purpose, so we may as well use it.  And while we're at it, it's often nice to use your own internal functions, so behavior is more consistent.
+    def isEmpty(self):
+        return (
+            self.size() == 0
+        )  # While there's nothing wrong with self.container == [], there is a builtin function for that purpose, so we may as well use it.  And while we're at it, it's often nice to use your own internal functions, so behavior is more consistent.
 
-     def push(self, item):
-         self.container.append(item)  # appending to the *container*, not the instance itself.
+    def push(self, item):
+        self.container.append(
+            item
+        )  # appending to the *container*, not the instance itself.
 
-     def pop(self):
-         element = self.container.pop()  # pop from the container, this was fixed from the old version which was wrong
-         #self.container[len(self.container)-1] = None
-         return element
+    def pop(self):
+        element = (
+            self.container.pop()
+        )  # pop from the container, this was fixed from the old version which was wrong
+        # self.container[len(self.container)-1] = None
+        return element
 
-     def peek(self):
-         if self.isEmpty():
-             raise Exception("Stack empty!")
-         return self.container[-1]  # View element at top of the stack
+    def pop2(self, pop2to):
+        start = pop2to
+        element = self.container.pop()
+        while start != element._label:
+            element = self.container.pop()
+        return element
 
-     def size(self):
-         return len(self.container)  # length of the container
+    def peek(self):
+        if self.isEmpty():
+            raise Exception("Stack empty!")
+        return self.container[-1]  # View element at top of the stack
 
-     def show(self):
-         return self.container  # display the entire stack as list
+    def size(self):
+        return len(self.container)  # length of the container
+
+    def show(self):
+        return self.container  # display the entire stack as list
+
 
 class RegExNode:
-    def __init__(self, label, children = []):
+    def __init__(self, label, children=[]):
         self._label = label
-        self._children.append(children)
+        self._children = children
+
 
 class TreeNode:
     def __init__(self, label, children):
         self._label = label
-        self._children.append(children)
+        self._children = children
         self._pattern = ""
         self._pos = 0
         self._stack = MyStack()
+
     def isStackEmpty(self):
         return len(self._stack) == 0
 
@@ -105,20 +124,31 @@ class TreeNode:
             if self.hasMoreChars() and self.peek() == "|":
                 self.match("|")
                 # Recurse
-                regex_node = RegExNode('expr', [])
+                regex_node = RegExNode("expr", [])
                 self._stack.push(regex_node)
                 expression = self.expr()
 
     def term2(self):
-        term = RegExNode('term')
+        term = RegExNode("term")
         self._stack.push(term)
         factor = self.factor2()
         if self.hasMoreChars() and self.peek() != "|":
             # Recurse 1
             self.term2()
+    def connect_stack(self):
+        topoffset = len(self._stack.container) - 1
+        topnode = self._stack.container[topoffset]
+        trgoffset = topoffset - 1
+        trgnode = self._stack.container[trgoffset]
+        copytopnode = copy.deepcopy(topnode)
+        copytrgnode = copy.deepcopy(trgnode)
+        trgnode = copytrgnode
+        trgnode._children.append(copytopnode)
+        self._stack.container[trgoffset] = trgnode
+        
 
     def factor2(self):
-        factor = RegExNode('factor')
+        factor = RegExNode("factor")
         self._stack.push(factor)
         atom = self.atom2()
         # before looking for meta, let's process stack
@@ -126,31 +156,23 @@ class TreeNode:
 
         try:
             while True:
-                topoffset = len(self._stack.container)-1
-                topnode = self._stack.container[topoffset]
-                trgoffset = topoffset-1
-                trgnode = self._stack.container[trgoffset]
-                trgnode.children.append(topnode)
+
+                self.connect_stack()
+                
                 currtop = self._stack.pop()
-                if currtop._label == 'factor':
+                
+                if currtop._label == "factor":
+                    self._stack.push(currtop)
                     exit
+
+            if self.hasMoreChars() and self.isMetaChar(self.peek()):
+                meta = self.next()
+                return TreeNode("Factor", [atom, TreeNode(meta, None)])
         except Exception as ex:
             print(ex)
-        while not currtop._label == 'factor':
-            peektop = self._stack.container[topoffset]
-            peektop._children.append(currtop)
-
-            currtop = self._stack.pop()
-
-            pass
-
-        if self.hasMoreChars() and self.isMetaChar(self.peek()):
-
-            meta = self.next()
-            return TreeNode("Factor", [atom, TreeNode(meta, None)])
 
     def atom2(self):
-        atom = RegExNode('atom')
+        atom = RegExNode("atom")
         self._stack.push(atom)
         if self.peek() == "(":
             self.match("(")
@@ -167,11 +189,14 @@ class TreeNode:
             raise ("Unexpected symbol = ".format(self.peek()))
         if self.peek() == "\\":
             self.match("\\")
-            return TreeNode("Char", [TreeNode("slash", "\\"), TreeNode("next", self.next())])
+            return TreeNode(
+                "Char", [TreeNode("slash", "\\"), TreeNode("next", self.next())]
+            )
 
         literal = RegExNode(self.next(), None)
-        char = RegExNode("Char", literal)
+        char = RegExNode("Char")
         self._stack.push(char)
+        self._stack.push(literal)
 
     # ===========================================================
     # Old Grammar Functions
@@ -222,19 +247,22 @@ class TreeNode:
             raise ("Unexpected symbol = ".format(self.peek()))
         if self.peek() == "\\":
             self.match("\\")
-            return TreeNode("Char", [TreeNode("slash", "\\"), TreeNode("next", self.next())])
+            return TreeNode(
+                "Char", [TreeNode("slash", "\\"), TreeNode("next", self.next())]
+            )
 
         return TreeNode("Char", [TreeNode(self.next(), None)])
 
     def any_char_except_meta(self):
         pass
 
+
 if __name__ == "__main__":
     expr = "ab*|ac"
     expr = "a+b?"
 
     tree_node = TreeNode("Begin", [])
-    regex_node = RegExNode('expr', [])
+    regex_node = RegExNode("expr", [])
     final_tree2 = tree_node.toParseTree2(expr, regex_node)
     print(final_tree2)
     # Old code
